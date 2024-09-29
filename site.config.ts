9ef884f73eb845b100b93b7d261823db34073def
Yoddh4 <97244305+Yoddh4@users.noreply.github.com>
2024-09-29 22:00:05 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -22,7 +22,7 @@ export default siteConfig({
   linkedin: 'venugopalan-a-k',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: 'KalariVenu', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'KalariVenu', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
